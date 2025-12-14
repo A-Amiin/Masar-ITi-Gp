419a18857803e902aff87c01c15ec802506e8d87
A-Amiin <ahmed.m.aminn@gmail.com>
2025-12-14 22:07:51 +0200

    Add theme toggle functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
+import useInitTheme from "@/hooks/useInitTheme";
 
 function App() {
+const {toggleTheme} = useInitTheme();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen dark:bg-amber-700 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen dark:bg-black bg-gray-100">
+      <Button className=" fixed top-4 right-4 dark:bg-white " onClick={toggleTheme}>Toggle Theme</Button> 
       <div className="flex flex-col justify-between h-32 dark:text-white mt-8">
         <h1 className="text-3xl font-bold underline">
           Hello, World! ... My name is Ahmed Amin
