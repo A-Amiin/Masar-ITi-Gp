92ba2e4241f7c4c79a50bd596139fffa04c32ce8
A-Amiin <ahmed.m.aminn@gmail.com>
2025-12-15 13:33:33 +0200

    Creating the app router for the pages of the project

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,53 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// layouts
+import DashboardLayout from "@/layouts/DashboardLayout";
+
+// routes
+import ProtectedRoute from "@/routes/ProtectedRoute";
+
+// pages
+import Landing from "@/pages/Landing/Landing";
+import Login from "@/pages/Login/Login";
+import Dashboard from "@/pages/Dashboard/Dashboard";
+import Agents from "@/pages/Agents/Agents";
+import Customers from "@/pages/Customers/Customers";
+import Tasks from "@/pages/Tasks/Tasks";
+import Inventory from "@/pages/Inventory/Inventory";
+import Orders from "@/pages/Orders/Orders";
+import Reports from "@/pages/Reports/Reports";
+import Chat from "@/pages/Chat/Chat";
+import Settings from "@/pages/Settings/Settings";
+import NotFound from "@/pages/NotFound/NotFound";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Public */}
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Admin */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<DashboardLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/agents" element={<Agents />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
