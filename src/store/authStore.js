ebb5080a687fd629f8a26e9b99411d24c5bb8924
A-Amiin <ahmed.m.aminn@gmail.com>
2025-12-15 13:35:31 +0200

    implimenting the auth store and the protected route for seperating the pages structure

---
+++ src/store/authStore.js
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      token: null,      // Firebase ID Token
+      user: null,
+
+      login: ({ token, user }) =>
+        set({
+          token,
+          user,
+        }),
+
+      logout: () =>
+        set({
+          token: null,
+          user: null,
+        }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
+
+export default useAuthStore;
