c59e657712755d5eb01bbc42ac4319908e911bf0
A-Amiin <ahmed.m.aminn@gmail.com>
2026-01-05 19:26:07 +0200

    Enhance firestoreCRUD: Add conditional user handling in addItem and improve getItems and getItemById methods

--- src/utils/firestoreCRUD.js
+++ src/utils/firestoreCRUD.js
@@ -1,10 +1,33 @@
 import { db } from "@/lib/firebase";
-import { collection, addDoc, updateDoc, deleteDoc, doc, getDocs, getDoc } from "firebase/firestore";
+import { collection, addDoc, updateDoc, deleteDoc, doc, getDocs, getDoc, setDoc } from "firebase/firestore";
 
 export const firestoreCRUD = (collectionName) => ({
   addItem: async (data) => {
     const colRef = collection(db, collectionName);
-    await addDoc(colRef, data);
+
+    if (
+      collectionName === "users" &&
+      data.role === "representative" &&
+      data.user_Id
+    ) {
+      const { user_Id, ...rest } = data;
+
+      const docRef = doc(db, collectionName, user_Id);
+
+      await setDoc(docRef, {
+        ...rest,
+        user_Id: user_Id, 
+      });
+
+      return user_Id;
+    }
+
+    const docRef = await addDoc(colRef, {
+      ...data,
+      createdAt: new Date(),
+    });
+
+    return docRef.id;
   },
   updateItem: async (id, data) => {
     const docRef = doc(db, collectionName, id);
@@ -17,16 +40,22 @@ export const firestoreCRUD = (collectionName) => ({
   getItems: async () => {
     const colRef = collection(db, collectionName);
     const snapshot = await getDocs(colRef);
-    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    return snapshot.docs.map((docItem) => ({
+      id: docItem.id,
+      ...docItem.data(),
+    }));
   },
   getItemById: async (id) => {
     const docRef = doc(db, collectionName, id);
     const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      return { id: docSnap.id, ...docSnap.data() };
-    } else {
+    if (!docSnap.exists()) {
       throw new Error("No such document!");
     }
+    return {
+      id: docSnap.id,
+      ...docSnap.data(),
+    };
   },
 });
