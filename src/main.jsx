aa6a29fbe4f21bd3812ca773ef1492b88e17ea7d
A-Amiin <ahmed.m.aminn@gmail.com>
2026-01-11 14:38:54 +0200

    Change the font with readable one on arabic (Amiri)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "@fontsource/amiri";
+import "@fontsource/amiri/700.css";
+import "@fontsource/amiri/400-italic.css";
+import "@fontsource/amiri/700-italic.css";
 import App from './App.jsx'
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
