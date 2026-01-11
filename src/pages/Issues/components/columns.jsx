972bb23da6877cc75d155d80d209c0ea0650b2e8
A-Amiin <ahmed.m.aminn@gmail.com>
2026-01-11 12:57:14 +0200

    refactor(table): move row actions to separate component and fix hooks usage with delete confirmation dialog

--- src/pages/Issues/components/columns.jsx
+++ src/pages/Issues/components/columns.jsx
@@ -1,7 +1,4 @@
-import { Eye, Trash2 } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { useState } from "react"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
+import MessageActions from "@/components/components/MessageAction"
 import { Badge } from "@/components/ui/badge"
 
 export const getColumns = (onView, onDelete, handleMarkRead) => [{
@@ -81,57 +78,13 @@ export const getColumns = (onView, onDelete, handleMarkRead) => [{
 {
   id: "actions",
   header: () => <div className="text-center">الإجراءات</div>,
-  cell: ({ row }) => {
-    const [openDialog, setOpenDialog] = useState(false)
-
-    const handleView = async () => {
-      if (!row.original.isRead) {
-        await handleMarkRead?.(row.original.id)
-      }
-      onView?.(row.original.id)
-    }
-
-
-    return (
-      <div className="flex justify-center gap-2">
-        {/* View */}
-        <Button size="icon" variant="ghost" onClick={handleView}>
-          <Eye className="w-4 h-4" />
-        </Button>
-
-        {/* Delete */}
-        <Button size="icon" variant="ghost" onClick={() => setOpenDialog(true)}>
-          <Trash2 className="w-4 h-4 text-red-500" />
-        </Button>
-
-        {/* Confirm Dialog */}
-        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-          <DialogContent dir="rtl">
-            <DialogHeader>
-              <DialogTitle>تأكيد الحذف</DialogTitle>
-            </DialogHeader>
-
-            <p className="py-4">
-              هل أنت متأكد أنك تريد حذف رسالة
-              <strong> {row.original.name} </strong>؟
-            </p>
-
-            <DialogFooter className="flex gap-2">
-              <Button variant="outline" onClick={() => setOpenDialog(false)}>إلغاء</Button>
-              <Button
-                variant="destructive"
-                onClick={() => {
-                  onDelete?.(row.original.id)
-                  setOpenDialog(false)
-                }}
-              >
-                حذف
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
-    )
-  },
+  cell: ({ row }) => (
+    <MessageActions
+      row={row}
+      onView={onView}
+      onDelete={onDelete}
+      handleMarkRead={handleMarkRead}
+    />
+  ),
 },
 ]
